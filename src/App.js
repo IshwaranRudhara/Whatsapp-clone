ef1006c71c4d153e5506cca5364899203d9fa79a
Ishwaran <39009926+IshwaranRudhara@users.noreply.github.com>
2023-01-04 21:25:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://github.com/IshwaranRudhara/react-app-1"
+          href="https://github.com/IshwaranRudhara"
           target="_blank"
           rel="noopener noreferrer"
         >
