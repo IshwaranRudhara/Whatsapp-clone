6796f95a809f2706317a8507439ae93212a911a4
Ishwaran <39009926+IshwaranRudhara@users.noreply.github.com>
2023-01-04 21:16:19 +0530

    commit 1

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           <code> Ishu react </code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/IshwaranRudhara/react-app-1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
